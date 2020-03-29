5ff719495c2c93f4a90c2544473fa97a27231751
William McCotter <bj.mccotter@veteransunited.com>
2020-03-29 17:40:00 -0500

    sending back data to use in angular chart

--- api.py
+++ api.py
@@ -52,24 +52,17 @@ def create_app(test_config=None):
     def getGraph():
 
         print("INSIDE GET GRAPH")
+        returnData = []
+        
         months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        fig = go.Figure()
-        fig.add_trace(go.Bar(
-            x=months,
-            y=[20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
-            name='Primary Product',
-            marker_color='indianred'
-        ))
-        fig.add_trace(go.Bar(
-            x=months,
-            y=[19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
-            name='Secondary Product',
-            marker_color='lightsalmon'
-        ))
-        # Here we modify the tickangle of the xaxis, resulting in rotated labels.
-        fig.update_layout(barmode='group', xaxis_tickangle=-45)
-        # fig.show()
-        pio.write_html(fig, file='./html/customer.html', auto_open=False)
+
+        for num, month in enumerate(months, start=1):
+            returnData.append({
+                "name": month,
+                "value": num
+            })
+
+        return jsonify(returnData)
 
     return app
