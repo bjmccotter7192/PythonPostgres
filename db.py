9c4104419cd502a4cbce987c0f138813890c86a8
William McCotter <bj.mccotter@veteransunited.com>
2020-03-29 13:18:19 -0500

    testing heroku python

--- db.py
+++ db.py
@@ -1,5 +1,9 @@
 import psycopg2
+import os
 
 def connectToDb():
-    conn = psycopg2.connect(host="192.168.1.133", port = 5432, database="test", user="postgres", password="postgres")
+    conn = psycopg2.connect(host=os.environ['DB_HOST'], port = 5432, 
+                            database=os.environ['DB_DB'], 
+                            user=os.environ['DB_USER'], 
+                            password=os.environ['DB_PASSWORD'])
     return conn
