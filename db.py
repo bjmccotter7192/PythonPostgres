1f3337aae6d94d1515ad636817ee6ba4433cb07c
William <bjmccotter7192@gmail.com>
2020-04-18 16:22:56 -0700

    trying to fix the db issue with local host

--- db.py
+++ db.py
@@ -1,9 +1,19 @@
 import psycopg2
 import os
+import json
 
 def connectToDb():
-    conn = psycopg2.connect(host=os.environ['DB_HOST'], port = 5432, 
-                            database=os.environ['DB_DB'], 
-                            user=os.environ['DB_USER'], 
-                            password=os.environ['DB_PASSWORD'])
+    if os.environ.get('DB_HOST'):
+        conn = psycopg2.connect(host=os.environ['DB_HOST'], port = 5432, 
+                                database=os.environ['DB_DB'], 
+                                user=os.environ['DB_USER'], 
+                                password=os.environ['DB_PASSWORD'])
+    else:
+        with open("config.json") as c:
+            config = json.load(c)
+
+        conn = psycopg2.connect(host=config.config['DB_HOST'], port = 5432, 
+                                database=config.config['DB_DB'], 
+                                user=config.config['DB_USER'], 
+                                password=config.config['DB_PASSWORD'])
     return conn
