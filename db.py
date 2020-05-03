239b588ce11e414adbb6afede0a55aedf1ad34f3
William McCotter <bj.mccotter@veteransunited.com>
2020-05-03 10:39:44 -0500

    switching back to heroku db

--- db.py
+++ db.py
@@ -3,13 +3,13 @@ import os
 import json
 
 def connectToDb():
-    # conn = psycopg2.connect(host=os.environ['DB_HOST'], port = 5432, 
-    #                         database=os.environ['DB_DB'], 
-    #                         user=os.environ['DB_USER'], 
-    #                         password=os.environ['DB_PASSWORD'])
+    conn = psycopg2.connect(host=os.environ['DB_HOST'], port = 5432, 
+                            database=os.environ['DB_DB'], 
+                            user=os.environ['DB_USER'], 
+                            password=os.environ['DB_PASSWORD'])
 
-    conn = psycopg2.connect(host="localhost", port = 5432, 
-                            database="test", 
-                            user="postgres", 
-                            password="postgres")
+    # conn = psycopg2.connect(host="localhost", port = 5432, 
+    #                         database="test", 
+    #                         user="postgres", 
+    #                         password="postgres")
     return conn
